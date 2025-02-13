84f3041a6d986dd32424a2c6da44d6bcf06b725c
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-02-13 16:10:12 +0100

    Adds completed exercise

---
+++ U4-S1-D4/Models/Utente.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_S1_D4.Models
+{
+    public class Utente
+    {
+        private string Username { get; set; }
+        private string Password { get; set; }
+        public bool IsLogged { get; set; } = false;
+        private DateTime DataOraLogin { get; set; }
+        private List<DateTime> Accessi { get; set; } = new List<DateTime>();
+
+        public Utente(string username, string password)
+        {
+            Username = username;
+            Password = password;
+      
+        }
+
+        public void Login(string Username,string Password) { 
+            if (Username == this.Username && Password == this.Password)
+            {
+                IsLogged = true;
+                Console.WriteLine("\r\nLogin effettuato con successo!");
+                DataOraLogin = DateTime.Now;
+                Accessi.Add(DataOraLogin);
+            }
+            else
+            {
+                Console.WriteLine("\r\nUsername o password errati!");
+            }
+        }
+
+
+        public void Logout()
+        {
+            if (IsLogged)
+            {
+                IsLogged = false;
+                Console.WriteLine("\r\nLogout effettuato con successo!");
+            }
+            else
+            {
+                Console.WriteLine("\r\nNon sei loggato!");
+
+            }
+        }
+
+        public void VerificaAccesso() {
+            if (IsLogged)
+            {
+                Console.WriteLine($"\r\nData e ora di login: {DataOraLogin}");
+            }
+            else
+            {
+                Console.WriteLine("\r\nNon sei loggato!");
+            }
+
+        }
+
+        public void ListaAccessi() {
+            if (IsLogged)
+            {
+                Console.WriteLine("\r\nLista degli accessi:");
+                foreach (var accesso in Accessi)
+                {
+                    Console.WriteLine(accesso);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\r\nNon sei loggato!");
+            }
+
+        }
+    }
+
+}
--- U4-S1-D4/Program.cs
+++ U4-S1-D4/Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using U4_S1_D4.Models;
+
+Utente utente1 = new Utente("mario.rossi", "123456");
+
+OperazioniAccesso(utente1);
+
+static void OperazioniAccesso(Utente utente)
+{
+    Console.WriteLine("\r\n===============OPERAZIONI==============\r\nScegli l'operazione da effettuare:\r\n1.: Login\r\n2.: Logout\r\n3.: Verifica ora e data di login\r\n4.: Lista degli accessi\r\n5.: Esci\r\n=======================================\r\n");
+    string scelta = Console.ReadLine();
+    if (scelta == "1")
+    {
+        if (utente.IsLogged == false)
+        {
+            Console.Write("\r\nInserisci username: ");
+            string username = Console.ReadLine();
+            Console.Write("\r\nInserisci password: ");
+            string password = Console.ReadLine();
+            utente.Login(username, password);
+            OperazioniAccesso(utente);
+        }
+        else
+        {
+            Console.WriteLine("\r\nSei già loggato!");
+            OperazioniAccesso(utente);
+        }
+    }
+    else if (scelta == "2")
+    {
+        utente.Logout();
+        OperazioniAccesso(utente);
+    }
+    else if (scelta == "3")
+    {
+        utente.VerificaAccesso();
+        OperazioniAccesso(utente);
+    }
+    else if (scelta == "4")
+    {
+        utente.ListaAccessi();
+        OperazioniAccesso(utente);
+    }
+    else if (scelta == "5")
+    {
+        Console.WriteLine("\r\nArrivederci!");
+    }
+    else
+    {
+        Console.WriteLine("\r\nScelta non valida!");
+        OperazioniAccesso(utente);
+    }
+}
